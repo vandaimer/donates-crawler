efa004fd139e0f8755d761855c7e7f4f995fd54a
Luiz Filipe <moresco.luiz@gmail.com>
2019-07-21 16:50:17 -0300

    Improve the code to crawler each vakinha

--- src/src/spiders/vakinha.py
+++ src/src/spiders/vakinha.py
@@ -7,6 +7,7 @@ class VakinhaSpider(scrapy.Spider):
     name = 'vakinha'
     allowed_domains = ['vakinha.com.br']
     start_urls = ['https://www.vakinha.com.br/vaquinhas/explore']
+    date_regex = '\d{2}/\d{2}/\d{4}'
     custom_settings = {
         'FEED_URI': '%(name)s_%(time)s.json',
         'FEED_FORMAT': 'json',
@@ -21,7 +22,7 @@ class VakinhaSpider(scrapy.Spider):
 
     def parse(self, response):
         list_of_items = response.css('.vakinhas-populares>.vakinha-item')
-        for item in list_of_items:
+        for item in list_of_items[:1]:
             link = item.css('a::attr(href)').get()
             title = item.css('h2::text').get()
             goal = item.css('.goal>span::text').get()
@@ -29,13 +30,29 @@ class VakinhaSpider(scrapy.Spider):
             collected = item.css('.collected>span::text').get()
             collected = re.sub(r'/\n|\s', '', collected)
 
-            yield {
+            item = {
                 'link': link,
                 'title': title,
                 'goal': goal,
                 'collected': collected,
             }
 
+            yield scrapy.Request(response.urljoin(link), self.parse_item, meta={ 'item': item })
+
+
         next_page = response.css('.pagination>li>a[rel="next"]::attr(href)').get()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), self.parse)
+
+    def parse_item(self, response):
+        item = response.meta.get('item')
+
+        item_id = response.css('#vakinha-id::text').get()
+        created_at = response.css('.created-at')[0].get()
+        end_at = response.css('.ending-at')[0].get()
+
+        item['id'] = int(re.findall('\d+', item_id).pop())
+        item['created_at'] = re.findall(VakinhaSpider.date_regex, created_at).pop()
+        item['end_at'] = re.findall(VakinhaSpider.date_regex, end_at).pop()
+
+        yield item
